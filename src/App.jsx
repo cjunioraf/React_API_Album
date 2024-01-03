0a211ba1243f56945d843e219bcd8896d6503ebf
cjunioraf <clecio,ferreira@gmail.com>
2024-01-03 18:58:05 -0300

    CONCLUSÃO DO PROJETO

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,50 @@ function App() {
   const [categoria, setCategoria] = useState("");
   const [fotos, setFotos] = useState([]);
   const [fotoAmpli, setFotoAmpli] = useState(null);
+  const [activeSearch, setActiveSearch] = useState(false);
 
   //função
   const fetchData = async({query, categoria}) => {
-
+    
     const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
-    const resp = await axios.get('https://api.unsplash.com/photos/random', {params:{client_id: apiKey, count: 12}});  
-    setFotos(resp.data);    
-    //console.log(resp);
+
+    if(query || categoria){
+
+      let searchQuery = query;
+
+      if(query && categoria){
+        searchQuery = `${query} ${categoria}`;
+      } else if(categoria){
+        searchQuery = categoria;          
+      } 
+      
+      const resp = await axios.get('https://api.unsplash.com/search/photos', {params:{client_id: apiKey, query: searchQuery}});          
+      setFotos(resp.data.results);
+
+    } else{
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {params:{client_id: apiKey, count: 12}});  
+      setFotos(resp.data);
+    }
+
   }
 
   useEffect(() => {
     fetchData(query, categoria);      
   }, [])
 
+  useEffect(() => {
+
+    if(activeSearch){
+      fetchData({query, categoria});
+      setActiveSearch(false);
+    }
+
+  }, [activeSearch])
+
 
   return (
     <div className='container'>
-      <Searchbar />  
+      <Searchbar setQuery={setQuery} setCategoria={setCategoria} setActiveSearch={setActiveSearch}/>  
       <FotoList fotos={fotos} setFotoAmpli={setFotoAmpli} />
       {fotoAmpli && 
         <FotoAmpliada foto={fotoAmpli} setFotoAmpli={setFotoAmpli} />
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-
-const Searchbar = () => {
+const Searchbar = ({setQuery, setCategoria, setActiveSearch}) => {
 
     const categorias = ["Natureza", "Pessoas", "Carros", "Animais", "Esportes", "Tecnologia"]
 
-  return (
+  return ( 
+
     <div className='search_bar'>
-        <input type="text" placeholder='Pesquisar Fotos...' />
-        <button>Perquisar</button>
-        <select>
+        <input type="text" placeholder='Pesquisar Fotos...' onChange={(e) => setQuery(e.target.value)} />
+        <button onClick={() => setActiveSearch(true)}>Perquisar</button>
+        <select onChange={(e) => 
+            {setCategoria(e.target.value); 
+             setActiveSearch(true);}}>
             {categorias.map((categoria) => (
                 <option key={categoria} 
                         value={categoria}>
